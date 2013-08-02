234161da50d390760d3a53b8653a9d483d77aa13
Karel Blaha <kblaha@daptiv.com>
2013-08-02 15:31:56 -0700

    Create and call logger
    Call logger methods while no listeners (e.g. appenders) are configured yet.

--- Program.cs
+++ Program.cs
@@ -1,15 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
+using log4net;
 
 namespace Sample.Log4Net
 {
     class Program
     {
-        static void Main(string[] args)
+        private static readonly ILog log =
+            LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        static void Main()
         {
+            log.Debug("Debug logging");
+            log.Info("Info logging");
+            log.Warn("Warning logging");
+            log.Error("Error logging");
+            log.Fatal("Fatal logging");
+
+            bool debugEnabled = log.IsDebugEnabled;
+            bool infoEnabled = log.IsInfoEnabled;
+            bool warnEnabled = log.IsWarnEnabled;
+            bool errorEnabled = log.IsErrorEnabled;
+            bool fatalEnabled = log.IsFatalEnabled;
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Log Levels Enabled:");
+            Console.ResetColor();
+
+            const string outputFormat = "\t{0}:\t{1}";
+
+            Console.WriteLine(outputFormat, "Debug", debugEnabled);
+            Console.WriteLine(outputFormat, "Info", infoEnabled);
+            Console.WriteLine(outputFormat, "Warn", warnEnabled);
+            Console.WriteLine(outputFormat, "Error", errorEnabled);
+            Console.WriteLine(outputFormat, "Fatal", fatalEnabled);
         }
     }
 }
